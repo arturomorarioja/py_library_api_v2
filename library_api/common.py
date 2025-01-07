0ac0aca47f832daf3cda1061d7b71194258b759e
Arturo Mora-Rioja <arturomora@gmail.com>
2025-01-07 19:47:54 +0100

    GET /books and GET /admin/books now account for the book cover API being down

--- library_api/common.py
+++ library_api/common.py
@@ -1,5 +1,6 @@
 import os
 import requests
+from flask import current_app
 from library_api.database import get_db
 from library_api.utils import error_message, convert_to_html_entities
 
@@ -24,17 +25,25 @@ def basic_book_info(book_id: int):
 
     if book == None:
         return error_message('Book not found')
-    else:        
-        # The book title is obtained from the book cover API
-        book_cover_base_url = os.getenv('BOOK_COVER_BASE_URL')
-        book_title = convert_to_html_entities(book['title'])
-        author_name = convert_to_html_entities(book['author'])
-        book_cover_url = f'{book_cover_base_url}?book_title={book_title}&author_name={author_name}'
-        result = dict(requests.get(book_cover_url).json())
-        if 'error' in result:
-            cover = ''
-        else:
-            cover = result['url']
+    else:
+        cover = ''
+
+        # The book cover is obtained from the book cover API
+        try:
+            book_cover_base_url = os.getenv('BOOK_COVER_BASE_URL')
+            book_title = convert_to_html_entities(book['title'])
+            author_name = convert_to_html_entities(book['author'])
+            book_cover_url = f'{book_cover_base_url}?book_title={book_title}&author_name={author_name}'
+
+            response = requests.get(book_cover_url, timeout=5)
+            response.raise_for_status()
+
+            result = response.json()
+            if 'url' in result:
+                cover = result['url']
+
+        except requests.exceptions.RequestException as e:
+            current_app.logger.error(f"Failed to fetch book cover: {e}")
 
         # The sqlite row is converted to a dictionary
         book_info = {key: book[key] for key in book.keys()}
